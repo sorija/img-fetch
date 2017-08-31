eb3f65dca0a81f6d7e0c3d09464d3acbafa463fc
Joanna Gawełda <joan.gawelda@gmail.com>
2017-08-31 18:00:11 +0300

    nitial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Image Scraper
+
+A command line tool for scraping all image tags from website and saving the corresponding urls as a single file.
---
+++ img_fetch.py
@@ -0,0 +1,6 @@
+#    Finds and fetches all images on a webpage and stores the images on disk
+#    Writes a file on disk, which lists the URL’s of the images fetched
+
+def scrape_img(url):
+  """List of img's links found at the url."""
+  return []
