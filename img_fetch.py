dbecf06f8e355cccf8bb6012f2cee8bd41035896
Joanna Gawełda <joan.gawelda@gmail.com>
2017-09-01 17:01:41 +0300

    mplement scrape_img

--- img_fetch.py
+++ img_fetch.py
@@ -1,8 +1,15 @@
 #    Finds and fetches all images on a webpage and stores the images on disk
 #    Writes a file on disk, which lists the URL’s of the images fetched
 import requests
+from bs4 import BeautifulSoup
 
 def scrape_img(url):
   """List of img's links found at the url."""
-  contents = requests.get(url).text
-  return []
+  html_str = requests.get(url).text
+  # find all the img and then value of associated src tag
+  soup = BeautifulSoup(html_str, 'html.parser')
+  img_tags = soup.find_all('img')
+  img_srcs = []
+  for tag in img_tags:
+    img_srcs.append(tag['src'])
+  return img_srcs
--- unit_test.py
+++ unit_test.py
@@ -17,8 +17,8 @@ def mocked_requests(*args, **kwargs):
 class MyTestCase(unittest.TestCase):
   @mock.patch('requests.get', side_effect=mocked_requests)
   def test_scrape_img(self, mock_get):
-    expected = ["foo bar", "first img", "second img"]
-    actual = img_fetch.scrape_img("test/test-data/mock-page.html")
+    expected = ["foo", "bar", "spam"]
+    actual = img_fetch.scrape_img("test-data/mock-page.html")
     self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
